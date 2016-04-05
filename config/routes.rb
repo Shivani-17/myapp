60a1f8b104f36458d43e97c157dae5ef7f72051d
Shivani_17 <shivani.aggarwal85204@gmail.com>
2016-04-06 02:39:10 +0530

    finish signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  
-
+resources :users
+root to: 'static_pages#home'
 get 'home' => 'static_pages#home', as: :home
 get 'help' => 'static_pages#help', as: :help
 get 'about' => 'static_pages#about', as: :about
