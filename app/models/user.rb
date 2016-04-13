7c7d76d9db66963f6767ae9070881238ee312f10
Shivani-17 <shivani.aggarwal85204@gmail.com>
2016-04-13 22:19:24 +0530

    user rest complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,6 @@ end
                     uniqueness: { case_sensitive: false }
   
   validates :password, presence: true, length: { minimum: 6 }
+validates :password_confirmation, presence: true, length: { minimum: 6 }
+
 end
